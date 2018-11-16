626d0d8bf31c8aa8034f3803fd3583f4fb56fec5
Danielle Butters <daniellebutters@DaniellsMacBook.fc.ca>
2018-11-16 10:19:43 -0500

    lottie working example

---
+++ js/main.js
@@ -0,0 +1,21 @@
+(()=> {
+    // get a ref to the lottie container
+
+    const preloader = document.querySelector('.preloader');
+
+    let preloadAnim = bodymovin.loadAnimation({
+        wrapper : preloader,
+        animType : 'svg',
+        loop : true,
+        autoplay: false,
+        path : 'data/search.json'
+    });
+
+
+    function playAnimation() {
+        preloadAnim.play();
+    }
+
+
+    preloader.addEventListener('mouseover', playAnimation);
+})();
